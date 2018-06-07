c9b428d2e7ec201d04d7955967d94f6fc486aa64
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-06-07 17:58:42 +0200

    debug ci (not finding pyrcc5

--- docker/build.sh
+++ docker/build.sh
@@ -6,6 +6,12 @@ echo $PYENV_PYTHON_VERSION
 eval "$(pyenv init -)" 
 pyenv shell $PYENV_PYTHON_VERSION
 
+echo "PATH=$PATH"
+export PATH=/opt/qt/5.9/5.9.4/gcc_64/bin:$PATH
+
+echo "DISPLAY=$DISPLAY"
+export DISPLAY=:99
+
 echo Generation of ressources and translations
 python3 gen_resources.py
 python3 gen_translations.py --lrelease
